2eba6dbb6110de714cd6c2f8a0487abd40bfd8f7
tajemouti <tajemouti@gmail.com>
2023-09-01 09:41:04 +0100

    Delete unnecessary boilerplate files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import store from './store';
 import { Provider } from 'react-redux';
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
 );
